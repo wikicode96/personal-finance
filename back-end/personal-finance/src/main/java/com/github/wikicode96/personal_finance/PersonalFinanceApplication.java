7686bc4e7fd1bc5e1a55c165bf0ca23e11cacf1b
Carlos Soler García <wikicode96@gmail.com>
2024-07-13 14:59:06 +0200

    back-end and front-end blank projects

---
+++ back-end/personal-finance/src/main/java/com/github/wikicode96/personal_finance/PersonalFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.github.wikicode96.personal_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceApplication.class, args);
+	}
+
+}
---
+++ back-end/personal-finance/src/test/java/com/github/wikicode96/personal_finance/PersonalFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wikicode96.personal_finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
